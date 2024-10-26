d27210140253f756b2d7dfbb3e508b6d9ba87dbe
velopace739 <velopace7@gmail.com>
2024-10-26 10:48:36 +0800

    update home

---
+++ frontend/src/app/dashboard/Overview.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Overview = () => {
+  return (
+    <>
+      <main>
+        <div>
+          <p>Overview</p>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default Overview
